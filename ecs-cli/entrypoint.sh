79f603b508b18d1e696b74d1ec6da8507bae03ac
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-08-08 12:03:29 +0300

    Update to 0.3 version with embedded configure

--- ecs-cli/entrypoint.sh
+++ ecs-cli/entrypoint.sh
@@ -2,4 +2,15 @@
 
 ln -s /ecs-cli /bin/ecs-cli
 
+CLUSTER_NAME=${CLUSTER_NAME:-default}
+CLUSTER_REGION=${CLUSTER_REGION:-us-east-1}
+DEFAULT_LAUNCH_TYPE=${DEFAULT_LAUNCH_TYPE:-EC2}
+CONFIG_NAME=${CONFIG_NAME:-default}
+
+ecs-cli configure \
+    --cluster "$CLUSTER_NAME" \
+    --region "$CLUSTER_REGION" \
+    --default-launch-type "$DEFAULT_LAUNCH_TYPE" \
+    --config-name "$CONFIG_NAME"
+
 exec "$@"
