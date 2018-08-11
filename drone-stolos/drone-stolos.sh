2eec02e584b974d9a2102ec0e4d8ea2c89f3881f
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-08-12 02:54:38 +0300

    Add secrets to drone-stolos plugin

--- drone-stolos/drone-stolos.sh
+++ drone-stolos/drone-stolos.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
-if [ -z "$PLUGIN_STOLOS_URL" ]
+
+if [ -z "$PLUGIN_STOLOS_URL" ] || [ -z "$STOLOS_URL" ]
 then
-    echo "You have not set stolos_url value"
+    echo "You have not set stolos_url value or secret"
     exit 1
 fi
 
-if [ -z "$PLUGIN_USERNAME" ]
+if [ -z "$PLUGIN_USERNAME" ] || [ -z "$USERNAME" ]
 then
-    echo "You have not set username value"
+    echo "You have not set username value or secret"
     exit 1
 fi
 
-if [ -z "$PLUGIN_PASSWORD" ]
+if [ -z "$PLUGIN_PASSWORD" ] || [ -z "$PASSWORD" ]
 then
-    echo "You have not set password value"
+    echo "You have not set password value or secret"
     exit 1
 fi
 
-if [ -z "$PLUGIN_PROJECT_UUID" ]
+if [ -z "$PLUGIN_PROJECT_UUID" ] || [ -z "$PROJECT_UUID" ]
 then
-    echo "You have not set project_uuid value"
+    echo "You have not set project_uuid value or secret"
     exit 1
 fi
 
+# Default secrets
+PLUGIN_STOLOS_URL=${PLUGIN_STOLOS_URL:=$STOLOS_URL}
+PLUGIN_USERNAME=${PLUGIN_USERNAME:=$USERNAME}
+PLUGIN_PASSWORD=${PLUGIN_PASSWORD:=$PASSWORD}
+PLUGIN_PROJECT_UUID=${PLUGIN_PROJECT_UUID:=$PROJECT_UUID}
+
+# Default file & service
 PLUGIN_FILE=${PLUGIN_FILE:-.stolos.yml}
 PLUGIN_BUILD_TARGET=${PLUGIN_BUILD_TARGET:-web}
 
